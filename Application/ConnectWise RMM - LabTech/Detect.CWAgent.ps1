c6d84cdb846e487d5cf81e2273eea876ed983f94
Redwanul Alam <redwan@gmail.com>
2023-10-04 10:41:19 +1100

    Add files via upload

--- Application/ConnectWise RMM - LabTech/Detect.CWAgent.ps1
+++ Application/ConnectWise RMM - LabTech/Detect.CWAgent.ps1
@@ -1,9 +1,17 @@
 $SEARCH = '*LabTech*'
-$INSTALLED = Get-ItemProperty HKLM:\Software\Wow6432Node\Google\Chrome\* | Select-Object DisplayName, UninstallString
-$INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, UninstallString
+$INSTALLED = Get-ItemProperty 'HKLM:\Software\Wow6432Node\LabTech\*' | Select-Object DisplayName, UninstallString
+$INSTALLED += Get-ItemProperty 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, UninstallString
 $RESULT = $INSTALLED | Where-Object { $_.DisplayName -like $SEARCH }
 
-if ($RESULT) {
+$SEARCH2 = '*ConnectWise Automate Remote Agent*'
+$INSTALLED2 += Get-ItemProperty 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, UninstallString
+$RESULT2 = $INSTALLED2 | Where-Object { $_.DisplayName -like $SEARCH2 }
+
+$SEARCH3 = '*ScreenConnect Client*'
+$INSTALLED3 += Get-ItemProperty 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, UninstallString
+$RESULT3 = $INSTALLED3 | Where-Object { $_.DisplayName -like $SEARCH3 }
+
+if ($RESULT -or $RESULT2 -or $RESULT3) {
     return 0
 } else {
     return 1
